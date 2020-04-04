9b909532fb007dfaba8d2b37968197b5bf833916
mdsalmankhan89 <mdsalmankhan89@gmail.com>
2020-04-04 12:29:17 +0530

    second

--- fileingest/filemanagement/urls.py
+++ fileingest/filemanagement/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns =[
    
-    path('',views.login,name='login') 
+    path('',views.index,name='index') 
 
  
 ]
--- fileingest/filemanagement/views.py
+++ fileingest/filemanagement/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def login(request):
-    return render(request,'login.html')
+def index(request):
+    return render(request,'index.html')
