a14337013245c1489adaf619c483266097b822fb
Priyanshu Goel <Priyanshu.Goel@factset.com>
2020-04-05 11:20:13 +0530

    login page

--- fileingest/account/urls.py
+++ fileingest/account/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 from . import views 
 
-urlpatterns =[
-   
-  #  path('register',views.register,name='register') ,
-  #  path('login',views.login,name='login') ,
-  #   path('logout',views.logout,name='logout') 
+urlpatterns = [
+	path('login', views.login, name='login'),
+	path('logout', views.logout, name='logout'),
+	path('upload', views.upload, name='upload')
 ]
--- fileingest/filemanagement/urls.py
+++ fileingest/filemanagement/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
 from . import views 
 
 urlpatterns =[
-   
-    path('',views.index,name='index') 
-
- 
+    path('',views.index,name='index'), 
+	path('admin/', admin.site.urls),
+	path('account/', include('account.urls'))
 ]
